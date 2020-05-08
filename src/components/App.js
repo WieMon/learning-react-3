e45b5e3ceb7d2feaea9b3d5c8295b9b2ed0a2d29
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-05-08 16:45:35 +0200

    Add the time counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import SwitchButton from './SwitchButton';
 
-function App() {
-  return (
-    <div>
-      Application
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    time: 0,
+    active: false
+  }
+
+  handleClick = () => {
+    if(this.state.active) {
+      clearInterval(this.idInterval)
+    } else {
+      this.idInterval = setInterval(() => this.addSecond(), 1000)
+    }
+
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
+  addSecond = () => {
+    this.setState({
+      time: this.state.time + 1
+    })
+  
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleClick} active={this.state.active}/>
+      </div>
+    )
+  }
 }
 
 export default App;
